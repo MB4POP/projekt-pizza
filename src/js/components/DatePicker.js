0b85d9bc0a43e04d6f55a8db6cb2c781079d0b20
Mariusz Białecki <mariusz.bialecki@onet.pl>
2019-09-11 16:07:28 +0200

    add get data from API, aggregate date

--- src/js/components/DatePicker.js
+++ src/js/components/DatePicker.js
@@ -31,7 +31,7 @@ class DatePicker extends BaseWidget {
           return (date.getDay() === 1);
         }
       ],
-      onClose: function(selectedDates, dateStr) {
+      onChange: function(selectedDates, dateStr) {
         thisWidget.value = dateStr;
       },
     });
